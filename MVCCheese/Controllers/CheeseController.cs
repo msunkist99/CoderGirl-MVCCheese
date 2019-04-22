a4e8c2e592be725a97e69b8d13fd34057553db05
msunkist99 <msunkist99@gmail.com>
2019-04-22 14:33:13 -0500

    20190422 Initial code

---
+++ MVCCheese/Controllers/CheeseController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCheese.Controllers
+{
+    public class CheeseController : Controller
+    {
+        static private List<string> Cheeses = new List<string>();
+
+        public IActionResult Index()
+        {
+            ViewBag.cheeses = Cheeses;
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Cheese/Add")]
+        public IActionResult NewCheese(string name)
+        {
+            Cheeses.Add(name);
+
+            return Redirect("/Cheese");
+        }
+    }
+}
