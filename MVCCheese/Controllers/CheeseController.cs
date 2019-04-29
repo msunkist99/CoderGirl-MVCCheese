c4b7bfd0abc5ca0d32a822fc0bc721db47b2d431
msunkist99 <msunkist99@gmail.com>
2019-04-29 18:39:07 -0500

    2019/04/29 add Cheese class

--- MVCCheese/Controllers/CheeseController.cs
+++ MVCCheese/Controllers/CheeseController.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MVCCheese.Models;
 
 namespace MVCCheese.Controllers
 {
     public class CheeseController : Controller
     {
         static private List<string> Cheeses = new List<string>();
+        static private Dictionary<string, string> CheesesDictionary = new Dictionary<string, string>();
+        static private List<Cheese> ListOfCheeseClasses = new List<Cheese>();
 
         public IActionResult Index()
         {
             ViewBag.cheeses = Cheeses;
+            ViewBag.cheesesDictionary = CheesesDictionary;
+            ViewBag.cheesesListOfCheese = ListOfCheeseClasses;
             return View();
         }
 
@@ -20,12 +25,20 @@ namespace MVCCheese.Controllers
         {
             return View();
         }
+        
 
         [HttpPost]
         [Route("/Cheese/Add")]
-        public IActionResult NewCheese(string name)
+        public IActionResult NewCheese(string name, string description)
         {
             Cheeses.Add(name);
+            CheesesDictionary.Add(name, description);
+
+            Cheese cheese = new Cheese();
+            cheese.Name = name;
+
+            cheese.Description = description;
+            ListOfCheeseClasses.Add(cheese);
 
             return Redirect("/Cheese");
         }
---
+++ MVCCheese/Models/Cheese.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCheese.Models
+{
+    public class Cheese
+    {
+        string name;
+        string description;
+
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Description {
+            get { return description; }
+            set { description = value; } }
+    }
+}
